de5a8026e5f98e50d97f4f2f623dcc35d6345be2
Abhishek Ghosh <abhighosh18@gmail.com>
2017-03-28 15:11:32 -0700

    Add Slack config

--- config.js
+++ config.js
@@ -1,20 +1,21 @@
-module.exports = {
-  // your community or team name to display on join page.
-  community: process.env.COMMUNITY_NAME || 'YOUR-TEAM-NAME',
-  // your slack team url (ex: socketio.slack.com)
-  slackUrl: process.env.SLACK_URL || 'YOUR-TEAM.slack.com',
-  // access token of slack
-  // You can generate it in https://api.slack.com/web#auth
-  // You should generate the token in admin user, not owner.
-  // If you generate the token in owner user, missing_scope error will be occurred.
-  //
-  // You can test your token via curl:
-  //   curl -X POST 'https://YOUR-SLACK-TEAM.slack.com/api/users.admin.invite' \
-  //   --data 'email=EMAIL&token=TOKEN&set_active=true' \
-  //   --compressed
-  slacktoken: process.env.SLACK_TOKEN || 'YOUR-ACCESS-TOKEN',
-  // an optional security measure - if it is set, then that token will be required to get invited.
-  inviteToken: process.env.INVITE_TOKEN || null,
+module.exports = {
+  // your community or team name to display on join page.
+  community: process.env.COMMUNITY_NAME || 'CS-6320',
+  // your slack team url (ex: socketio.slack.com)
+  slackUrl: process.env.SLACK_URL || 'cs6320.slack.com',
+  // access token of slack
+  // You can generate it in https://api.slack.com/web#auth
+  // You should generate the token in admin user, not owner.
+  // If you generate the token in owner user, missing_scope error will be occurred.
+  //
+  // You can test your token via curl:
+  //   curl -X POST 'https://YOUR-SLACK-TEAM.slack.com/api/users.admin.invite' \
+  //   --data 'email=EMAIL&token=TOKEN&set_active=true' \
+  //   --compressed
+  slacktoken: process.env.SLACK_TOKEN || 'xoxp-161793762167-160393960320-160435676209-b5617e030621f1fa6739dbc43bf3175d',
+  // an optional security measure - if it is set, then that token will be required to get invited.
+  inviteToken: process.env.INVITE_TOKEN || null,
 
-  locale: process.env.LOCALE || "en",
+  locale: process.env.LOCALE || "en",
 };
+
